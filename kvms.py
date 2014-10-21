c0374816e9f7bdcc1a5fde610029fc77de9ee999
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2014-10-21 04:05:30 +0000

    Add shutdown a domain.

--- kvms.py
+++ kvms.py
@@ -11,3 +11,11 @@ if sys.argv[0] == 'list':
     domains = conn.listAllDomains()
     for domain in domains:
         print domain.name()
+
+if sys.argv[0] == 'shutdown':
+    domain_name = sys.argv[1]
+    domain = conn.lookupByName(domain_name)
+    if (domain == None):
+        print 'Domain not found.'
+        sys.exit(1)
+    domain.shutdown()
