99e45a7d899e1612d509b5a7c05c152d27a02dbe
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2014-10-21 04:36:41 +0000

    Improve listing of domains.

--- kvms.py
+++ kvms.py
@@ -2,6 +2,8 @@
 import libvirt
 import sys
 
+state = {0: 'No state', 1: 'Running', 2: 'Blocked', 3: 'Paused', 4: 'Being shut down', 5: 'Shut off', 6: 'Crashed', 7: 'Suspended'}
+
 conn = libvirt.openReadOnly(None)
 if conn == None:
     print 'Failed to open connection to the hypervisor'
@@ -9,8 +11,9 @@ if conn == None:
 
 if sys.argv[0] == 'list':
     domains = conn.listAllDomains()
+    print 'ID\tDomain name\tState'
     for domain in domains:
-        print domain.name()
+        print str(domain.ID()) + '\t' +  domain.name() + '\t' + state[domain.state()[0]]
 
 if sys.argv[0] == 'shutdown':
     domain_name = sys.argv[1]
