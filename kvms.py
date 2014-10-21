28a3be28dec22334831c0bf796774b70cb72055b
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2014-10-21 05:14:45 +0000

    Fix domain not found.

--- kvms.py
+++ kvms.py
@@ -9,29 +9,43 @@ if conn == None:
     print 'Failed to open connection to the hypervisor'
     sys.exit(1)
 
-if sys.argv[0] == 'list':
-    domains = conn.listAllDomains()
+def shutdownByName(name):
+    try:
+        domain = conn.lookupByName(domain_name)
+    except:
+        return False
+    domain.shutdown()
+    return True
+
+def shutdownAll():
+    try:
+        domains = conn.listAllDomains() 
+    except:
+        return
+
+    for domain in domains:
+        domain.shutdown()
+
+if sys.argv[1] == 'list':
+    try:
+        domains = conn.listAllDomains()
+    except:
+        print "No domain found."
+        sys.exit(1)
+
     print 'ID\tDomain name\tState'
     for domain in domains:
         print str(domain.ID()) + '\t' +  domain.name() + '\t' + state[domain.state()[0]]
 
-if sys.argv[0] == 'shutdown':
-    domainName = sys.argv[1]
-    found = shutdownByName(domainName)
-    if (!found):
-        print 'Domain not found.'
-        sys.exit(1)
+if sys.argv[1] == 'shutdown':
+    domainName = sys.argv[2]
 
-def shutdownByName(name):
-    domain = conn.lookupByName(domain_name)
-    if (domain == None):
-        return false
-    domain.shutdown()
-    return true
-
-def shutdownAll():
-    domains = conn.listAllDomains()    
-    for domain in domains:
-        domain.shutdown()
+    if (domainName == 'all'):
+        shutdownAll()
+    else:
+        found = shutdownByName(domainName)
+        if (not found):
+            print 'Domain ' + str(domainName) + ' was not found.'
+            sys.exit(1)
 
 
