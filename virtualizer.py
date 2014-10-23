9ab5540adab0ddf429d07f6ff9de77f10efd8398
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2014-10-23 04:06:52 +0000

    Add start and reboot.

--- virtualizer.py
+++ virtualizer.py
@@ -10,6 +10,15 @@ class Virtualizer:
             print 'Failed to open connection to the hypervisor'
             sys.exit(1)
 
+    def start(self, domain):
+        domain.create()
+
+    def reboot(sef, domain):
+        domain.reboot()
+
+    def shutdown(self, domain):
+        domain.shutdown()
+
     def shutdown_by_name(self, domain_name):
         domain = get_domain_by_name(domain_name)
         if (domain == None):
@@ -50,8 +59,12 @@ class Virtualizer:
             raise RuntimeError("The memory can not be changed for " + domain.name())
 
     def set_domain_vcpus(self, domain, vcpus):
+        maxVcpus = domain.maxVcpus
+        if (vcpus > domain.maxVcpus):
+            raise RuntimeError("The vcpus is greater than max allowed (" + maxVcpus + ")")
         if (domain.vcpus() != vcpus):
             try:
                 domain.setVcpus(vcpus)
             except:
                 raise RuntimeError("The vcpus could not be changed for " + domain.name())
+
